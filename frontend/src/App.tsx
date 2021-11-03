16af446448badde3f4c03313b842e5128bd49e98
Diego Lima <limarodrigues_diego@hotmail.com>
2021-11-02 21:28:27 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,20 @@
-function App() {
-  return (
-    <div>
-      <h1 className="text-primary">Olá Mundo!</h1>
-    </div>
-  );
-}
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
 
-export default App;
+  function App() {
+    return (
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá Mundo!</h1>
+
+        <DataTable />
+        
+      </div>
+      <Footer />
+    </>
+    );
+  }
+
+  export default App;
